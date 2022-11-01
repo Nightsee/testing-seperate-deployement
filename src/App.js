8ca96559b9adf895b359aafd3346f9d6adf98af1
Nightsee <younes.naitsi.2002e@gmail.com>
2022-11-01 18:36:26 +0100

    aa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import axios from "axios";
 import './App.css';
+import {useState ,useEffect} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () =>{
+  const [data, setdata] = useState('');
+  useEffect( 
+    ()=>{
+      axios.get('https://testing-seperate-deployement.herokuapp.com/').then((res) => {
+        setdata(res.data);
+      })
+    }
+    ,[])
+
+  return(
+    <div>
+      <h1>frontend working</h1> 
+      <h3>the data recieved from the backend is: {data}</h3> 
     </div>
-  );
+  )
 }
 
 export default App;
